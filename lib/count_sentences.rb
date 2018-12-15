1c5ad08c4587af744798faa32f86c6474a207af1
Bennimaru <bennywhyu@gamil.com>
2018-12-15 17:16:07 -0500

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,25 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.end_with?(".")
   end
 
   def question?
-
+    self.end_with?("?")
   end
 
   def exclamation?
-
+    self.end_with?("!")
   end
 
   def count_sentences
-
+    arr_of_sentences= []
+    arr_of_sentences= self.split(/\.|\?|\!/)
+    arr_of_sentences.delete_if do |snip|
+      snip.length<2
+    end
+    arr_of_sentences.size
   end
+
+
 end
